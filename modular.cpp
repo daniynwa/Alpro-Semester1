0826b11d223c95974f339cf579f86529a352b6d0
Dani Afriyadi Yusuf <dani94andara@gmail.com>
2017-03-26 12:13:51 +0700

    tugas-26-03-2017-modular

---
+++ modular.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//Fungsi Garis dan nim
+
+garis ()
+{
+	cout << "----------------------------------" << endl;
+}
+nim ()
+{
+	cout << "16 111 286" << endl;
+}
+
+main ()
+//Untuk mengeluarkan fungsi garis dan nim
+{
+	garis ();
+	cout << "DANI AFRIYADI YUSUF" << endl;
+	nim ();
+	garis ();
+
+
+
+return 0;
+}
